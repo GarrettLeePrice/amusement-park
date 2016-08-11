df73cf6decfcc0b8a6bb1d873de9a9559d452854
dean scelza and garrett price <garrett@>
2016-08-11 10:34:44 -0700

    build input form, add jquery functionality for form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,13 @@
-$(document).ready(function(){
-
-  $("form#survey").submit(function(event){
-  var heightInput = parseInt("input#height");
-    $(".height").text(heightInput);
-
-    if (height < 36) {
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var heightInput = parseInt($("input#height").val());
+    if (heightInput < 36) {
       $('#rides1').show();
-    } else if (height < 48) {
-      $('#rides1').show();
+    } else if (heightInput < 48) {
       $('#rides2').show();
     } else {
-      $('#rides1').show();
-      $('#rides2').show();
       $('#rides3').show();
     }
-
     event.preventDefault();
-
   });
 });
